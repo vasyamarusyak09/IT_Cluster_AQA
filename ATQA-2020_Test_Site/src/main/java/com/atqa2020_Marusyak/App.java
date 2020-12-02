db1a33779d5c3a70e931a3c25a7d5e2860fb4201
vasyamarusyak09 <vasya_marusyak@i.ua>
2020-12-02 20:53:57 +0200

    created maven project

---
+++ ATQA-2020_Test_Site/src/main/java/com/atqa2020_Marusyak/App.java
@@ -0,0 +1,13 @@
+package com.atqa2020_Marusyak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ATQA-2020_Test_Site/src/test/java/com/atqa2020_Marusyak/AppTest.java
@@ -0,0 +1,20 @@
+package com.atqa2020_Marusyak;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
